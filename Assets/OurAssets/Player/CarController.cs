34404e42a60c8876ed9cb8d2041a6d0e60b23fe1
BenetManzanaresSalor <benetmanzanares@gmail.com>
2022-11-09 15:36:42 +0100

    Player: Added CarController, HandBrake, ABS, Impreza model and TextMeshPro

---
+++ Assets/OurAssets/Player/CarController.cs
@@ -0,0 +1,198 @@
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+using System;
+
+public abstract class CarController : MonoBehaviour
+{
+    [Header("Car wheels")]  // Assign wheel Colliders and transform through the inspector
+    [SerializeField] protected WheelCollider FrontLeftC;
+    [SerializeField] protected Transform FrontLeftT;
+    [SerializeField] protected WheelCollider FrontRightC;
+    [SerializeField] protected Transform FrontRightT;
+    [SerializeField] protected WheelCollider BackLeftC;
+    [SerializeField] protected Transform BackLeftT;
+    [SerializeField] protected WheelCollider BackRightC;
+    [SerializeField] protected Transform BackRightT;
+
+    [Header("Car body")]    // Assign a Gameobject representing the front of the car
+    [SerializeField] protected Rigidbody CarRigidBody;
+    [SerializeField] protected Transform CarFront;
+
+    [Header("General Parameters")]
+    [SerializeField] protected bool EnableMovement = true;
+    [SerializeField] protected bool EnableABS = true;
+    [SerializeField] protected float ABSThld = 0.75f;
+    [SerializeField] protected float MaxSteeringAngle = 45;
+    [SerializeField] protected float MaxSpeed = 200;
+    [SerializeField] protected float BrakeTorque = 5000;
+    [SerializeField] protected float MovementTorque = 1000;
+    [SerializeField] protected bool LimitRpmAtCurve = true;
+    [SerializeField] protected float CurveSteerAngleThld = 30;
+    [SerializeField] protected float MaxSpeedAtCurve = 100;
+
+    // Constants    
+    protected const float MPS_TO_KPH = 3600f / 1000f;
+    protected const float RPM_PER_RADIUS_TO_KPH = 1f / 60f * 2f * Mathf.PI;
+
+    // Auxiliar local variables
+    protected float LocalMaxSpeed;
+    
+
+    protected virtual void Start()
+    {
+        // Set center of mass to zero
+        CarRigidBody.centerOfMass = Vector3.zero;
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Steer();
+        Move();
+    }
+
+    /// <summary>
+    ///  Updates the wheel's postion and rotation
+    /// </summary>
+    protected virtual void UpdateWheels()
+    {
+        ApplyRotationAndPostion(FrontLeftC, FrontLeftT);
+        ApplyRotationAndPostion(FrontRightC, FrontRightT);
+        ApplyRotationAndPostion(BackLeftC, BackLeftT);
+        ApplyRotationAndPostion(BackRightC, BackRightT);
+    }
+
+    /// <summary>
+    /// Updates the wheel's postion and rotation
+    /// </summary>
+    /// <param name="targetWheel"></param>
+    /// <param name="wheel"></param>
+    protected virtual void ApplyRotationAndPostion(WheelCollider targetWheel, Transform wheel)
+    {
+        targetWheel.ConfigureVehicleSubsteps(5, 12, 15);
+
+        Vector3 pos;
+        Quaternion rot;
+        targetWheel.GetWorldPose(out pos, out rot);
+        wheel.position = pos;
+        wheel.rotation = rot;
+    }
+
+    protected abstract float GetSteeringAngle();
+
+    /// <summary>
+    /// Applies steering to the Current waypoint
+    /// </summary>
+    protected virtual void Steer()
+    {
+        // Get steering angle
+        float steeringAngle = GetSteeringAngle();
+
+        // Set direction wheels angle
+        FrontLeftC.steerAngle = steeringAngle;
+        FrontRightC.steerAngle = steeringAngle;
+
+        // If desired, adapt local maximum speed depending of the steering angle
+        if (LimitRpmAtCurve)
+            LocalMaxSpeed = (Mathf.Abs(steeringAngle) <= CurveSteerAngleThld) ? MaxSpeed : MaxSpeedAtCurve;
+        else
+            LocalMaxSpeed = MaxSpeed;
+
+        UpdateWheels();
+    }
+
+    protected virtual float GetWheelsSpeed()
+    {
+        // Average wheels speed
+        return (GetWheelSpeed(FrontLeftC) + GetWheelSpeed(FrontRightC) + GetWheelSpeed(BackLeftC) + GetWheelSpeed(BackRightC)) / 4;
+    }
+
+    protected virtual float GetWheelSpeed(WheelCollider wheel)
+    {
+        return wheel.rpm * wheel.radius * RPM_PER_RADIUS_TO_KPH;
+    }
+
+    protected virtual float GetForwardSpeed()
+	{
+        return Vector3.Dot(CarRigidBody.velocity, transform.forward) * MPS_TO_KPH;
+    }
+
+    /// <summary>
+    /// Apply brake torque 
+    /// </summary>
+    protected virtual void Brake(float brakeRatio = 1)
+    {
+        // Check difference between forward speed and wheels speed
+        if (EnableABS && brakeRatio > 0)
+		{
+            float absWheelsSpeed = Mathf.Abs(GetWheelsSpeed());
+            float forwardSpeed = Vector3.Dot(CarRigidBody.velocity, transform.forward);
+            float slipRatio = absWheelsSpeed / forwardSpeed;
+            if (slipRatio > 0.1 && slipRatio <= ABSThld)
+                brakeRatio = 0;
+        }
+
+        FrontLeftC.brakeTorque = brakeRatio * BrakeTorque;
+        FrontRightC.brakeTorque = brakeRatio * BrakeTorque;
+        BackLeftC.brakeTorque = brakeRatio * BrakeTorque;
+        BackRightC.brakeTorque = brakeRatio * BrakeTorque;
+    }
+
+    protected virtual void Accelerate(float accelRatio = 1)
+    {
+        BackRightC.motorTorque = accelRatio * MovementTorque;
+        BackLeftC.motorTorque = accelRatio * MovementTorque;
+        FrontRightC.motorTorque = accelRatio * MovementTorque;
+        FrontLeftC.motorTorque = accelRatio * MovementTorque;
+    }
+
+
+
+    protected abstract float GetMovementDirection();
+
+    /// <summary>
+    /// Moves the car forward and backward depending on the input
+    /// </summary>
+    protected virtual void Move()
+    {
+        // If movement allowed
+        if (EnableMovement)
+        {
+            // Reset brake and acceleration torque
+            Brake(0);
+            Accelerate(0);
+
+            // Get movement magnitude
+            float movementDirection = GetMovementDirection();
+
+            // Compute speed of wheels
+            float wheelsSpeed = GetWheelsSpeed();
+            float absWheelsSpeed = Mathf.Abs(wheelsSpeed);            
+
+            // If movement direction is different from wheels speed, brake
+            if ((movementDirection > 0 && wheelsSpeed < 0) || (movementDirection < 0 && wheelsSpeed > 0))
+            {
+               Brake(Mathf.Abs(movementDirection));    // Absolute braking (independently of the direction sign)
+            }
+            // Otherwise, normal acceleration
+            else
+            {
+                // If speed below local maximum, accelerate
+                if (absWheelsSpeed < LocalMaxSpeed)
+                    Accelerate(movementDirection);
+                // If speed is too high (with a 1/4 marging), brake
+                else if (absWheelsSpeed > LocalMaxSpeed + (LocalMaxSpeed * 1 / 4))
+                    Brake();
+                // Otherwise, do nothing
+            }
+        }
+        // If movement disabled, brake
+        else
+            Brake();
+    }
+
+
+}
--- Assets/OurAssets/Player/Player.cs
+++ Assets/OurAssets/Player/Player.cs
@@ -3,162 +3,77 @@ using System.Collections;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
-public class Player : MonoBehaviour
+public class Player : CarController
 {
-    [Header("Car wheels")]  // Assign wheel Colliders and transform through the inspector
-    public WheelCollider frontLeft;
-    public Transform wheelFL;
-    public WheelCollider frontRight;
-    public Transform wheelFR;
-    public WheelCollider backLeft;
-    public Transform wheelBL;
-    public WheelCollider backRight;
-    public Transform wheelBR;    
+    [SerializeField] protected bool EnableHandBrake = true;
+    [SerializeField] protected float HardBrakeStifnessMultiplier = 0.1f;
 
-    [Header("Car body")]    // Assign a Gameobject representing the front of the car
-    public Rigidbody CarRigidBody;
-    public Transform carFront;
+    [Header("Canvas")]
+    [SerializeField] protected TextMeshProUGUI SpeedText;
+    
 
-    [Header("General Parameters")]
-    public float MaxSteeringAngle = 45;
-    public float MaxSpeed = 150;
-    public float CurveAngleThld = 15;
-    public float MaxSpeedAtCurve = 100;
-    public float BrakeForce = 5000;
-    public float MovementTorque = 400;
-    public bool EnableMovement = true;
+    // Auxiliar variables
+    protected float BackWheelsOriginalStiffness;
+    protected WheelFrictionCurve BackWheelsFrictionCurve;
 
-    // Auxiliar local variables
-    private float LocalMaxSpeed;
+	protected override void Start()
+	{
+		base.Start();
 
-    protected void Start()
-    {
-        // Set center of mass to zero
-        CarRigidBody.centerOfMass = Vector3.zero;
+        // Get info from backwheels for hand brake
+        BackWheelsOriginalStiffness = BackLeftC.sidewaysFriction.stiffness;
+        BackWheelsFrictionCurve = BackLeftC.sidewaysFriction;
     }
 
-    protected void FixedUpdate()
-    {       
-        UpdateWheels();
-        Steer();
-        Move();
+	protected override void FixedUpdate()
+	{
+        base.FixedUpdate();
+        CheckHandBrake();
     }
 
-    /// <summary>
-    ///  Updates the wheel's postion and rotation
-    /// </summary>
-    protected void UpdateWheels()
-    {
-        ApplyRotationAndPostion(frontLeft, wheelFL);
-        ApplyRotationAndPostion(frontRight, wheelFR);
-        ApplyRotationAndPostion(backLeft, wheelBL);
-        ApplyRotationAndPostion(backRight, wheelBR);
+    protected virtual void CheckHandBrake()
+	{
+        if (EnableHandBrake)
+		{
+            float brakeRatio = Mathf.Abs(Input.GetAxis("Jump"));
+
+            // Change stiffness        
+            float newStiffness = Mathf.Lerp(BackWheelsOriginalStiffness,
+                BackWheelsOriginalStiffness * HardBrakeStifnessMultiplier,
+                brakeRatio);
+            BackWheelsFrictionCurve.stiffness = newStiffness;
+            BackLeftC.sidewaysFriction = BackWheelsFrictionCurve;
+            BackRightC.sidewaysFriction = BackWheelsFrictionCurve;
+
+            // Apply hand brake
+            HandBrake(brakeRatio);
+        }
+    }
+        
+
+    protected virtual void HandBrake(float brakeRatio)
+	{
+        BackLeftC.brakeTorque = brakeRatio * BrakeTorque;
+        BackRightC.brakeTorque = brakeRatio * BrakeTorque;
     }
 
-    /// <summary>
-    /// Updates the wheel's postion and rotation
-    /// </summary>
-    /// <param name="targetWheel"></param>
-    /// <param name="wheel"></param>
-    protected void ApplyRotationAndPostion(WheelCollider targetWheel, Transform wheel)
-    {
-        targetWheel.ConfigureVehicleSubsteps(5, 12, 15);
+    protected virtual void Update()
+	{
+        float wheelsSpeed = GetWheelsSpeed();
+        SpeedText.text = (int)wheelsSpeed + "km/h ";
+	}
 
-        Vector3 pos;
-        Quaternion rot;
-        targetWheel.GetWorldPose(out pos, out rot);
-        wheel.position = pos;
-        wheel.rotation = rot;
-    }
-
-    protected float GetSteeringAngle()
+    protected override float GetSteeringAngle()
 	{
         float steeringValue = Input.GetAxis("Horizontal");
         float steeringAngle = steeringValue * MaxSteeringAngle;
         return steeringAngle;
     }
 
-    /// <summary>
-    /// Applies steering to the Current waypoint
-    /// </summary>
-    protected void Steer()
-    {
-        // Get steering angle
-        float steeringAngle = GetSteeringAngle();
-
-        // Set direction wheels angle
-        frontLeft.steerAngle = steeringAngle;
-        frontRight.steerAngle = steeringAngle;
-
-        // Adapt local maximum speed depending of the steering angle
-        LocalMaxSpeed = (steeringAngle <= CurveAngleThld) ? MaxSpeed : MaxSpeedAtCurve;        
-    }
-
-    /// <summary>
-    /// Apply brake torque 
-    /// </summary>
-    protected void Brake(float brakeRatio=1)
-    {
-        frontLeft.brakeTorque = brakeRatio * BrakeForce;
-        frontRight.brakeTorque = brakeRatio * BrakeForce;
-        backLeft.brakeTorque = brakeRatio * BrakeForce;
-        backRight.brakeTorque = brakeRatio * BrakeForce;
-    }
-
-    protected void Accelerate(float accelRatio=1)
-	{
-        backRight.motorTorque = accelRatio * MovementTorque;
-        backLeft.motorTorque = accelRatio * MovementTorque;
-        frontRight.motorTorque = accelRatio * MovementTorque;
-        frontLeft.motorTorque = accelRatio * MovementTorque;
-    }
-
-    protected float GetMovementMagnitude()
+    protected override float GetMovementDirection()
 	{
         return Input.GetAxis("Vertical");
     }
-
-    /// <summary>
-    /// Moves the car forward and backward depending on the input
-    /// </summary>
-    protected void Move()
-    {
-        // If movement allowed
-        if (EnableMovement)
-        {
-            // Reset brake and acceleration torque
-            Brake(0);
-            Accelerate(0);
-
-            // Get movement magnitude
-            float movementMagnitude = GetMovementMagnitude();
-
-            // Compute average speed of wheels
-            int wheelsSpeed = (int)((frontLeft.rpm + frontRight.rpm + backLeft.rpm + backRight.rpm) / 4);
-            int absWheelsSpeed = Mathf.Abs(wheelsSpeed);
-
-            // If movement direction is different from wheels speed, brake
-            if ((movementMagnitude > 0 && wheelsSpeed < 0) || (movementMagnitude < 0 && wheelsSpeed > 0))
-			{
-                Brake(Mathf.Abs(movementMagnitude));    // Absolute braking
-            }
-            // Otherwise, normal acceleration
-			else
-			{
-                // If speed below local maximum, accelerate
-                if (absWheelsSpeed < LocalMaxSpeed)
-                    Accelerate(movementMagnitude);
-                // If speed is too high (with a 1/4 marging), brake
-                else if (absWheelsSpeed > LocalMaxSpeed + (LocalMaxSpeed * 1 / 4))
-                    Brake();
-                // Otherwise, do nothing
-            }            
-        }
-        // If movement disabled, brake
-        else
-            Brake();
-    }
-
-
 }
